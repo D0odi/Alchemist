01bb27360476af396c0ad288622a7e4311ecf841
Lev Natekin <natekinlev@gmail.com>
2024-04-19 18:11:57 -0700

    gimini response display, error handling

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,21 +1,21 @@
 import { motion } from "framer-motion";
-import { client } from "./api/client";
+import { generateItem, serverStatus } from "./api/client";
+import { useState } from "react";
 
 function App() {
-  const handleClick = async () => {
-    try {
-      const response = await client.get("/");
-      console.log(response.data.message);
-    } catch (error) {
-      console.error(error);
-    }
+  const [generatedItem, setGeneratedItem] = useState<string>("");
+
+  const handleGenerateItem = async () => {
+    const res = await generateItem();
+    if (res != null) setGeneratedItem(res);
+    else setGeneratedItem("Error generating the loot");
   };
   return (
     <div className="full-screen items-center justify-center flex flex-col bg-white">
       <div className="h-2/3 w-1/2 justify-center items-center flex flex-col bg-gray-500">
         <div className="size-full bg-black grid grid-cols-3">
-          <div className="col-span-2 flex justify-center items-center bg-green-500">
-            fdf
+          <div className="col-span-2 flex p-10 bg-green-500">
+            <p>{generatedItem}</p>
           </div>
           <div className="col-span-1 bg-orange-400 grid grid-rows-3">
             <div className="flex justify-center items-center">Title</div>
@@ -23,13 +23,22 @@ function App() {
             <div className="flex justify-center items-center">Rarity</div>
           </div>
         </div>
-        <motion.button
-          className="w-full h-28 bg-red-500"
-          whileHover={{ backgroundColor: "rgba(239, 68, 68, 0.7)" }}
-          onClick={() => handleClick()}
-        >
-          fdf
-        </motion.button>
+        <div className="flex flex-row w-full">
+          <motion.button
+            className="w-1/3 h-28 bg-red-500"
+            whileHover={{ backgroundColor: "rgba(239, 68, 68, 0.7)" }}
+            onClick={() => serverStatus()}
+          >
+            <span className="uppercase font-bold">Server Test</span>
+          </motion.button>
+          <motion.button
+            className="w-full h-28 bg-red-500"
+            whileHover={{ backgroundColor: "rgba(239, 68, 68, 0.7)" }}
+            onClick={() => handleGenerateItem()}
+          >
+            <span className="uppercase font-bold">Generate Item</span>
+          </motion.button>
+        </div>
       </div>
     </div>
   );
--- react-ts/src/api/client.ts
+++ react-ts/src/api/client.ts
@@ -2,4 +2,29 @@ import axios from "axios";
 
 const baseURL = "https://express-looter-ai.onrender.com";
 
-export const client = axios.create({ baseURL });
+const client = axios.create({ baseURL });
+
+const serverStatus = async (): Promise<void> => {
+  try {
+    const response = await client.get("/");
+    console.log(response.data.message);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const generateItem = async (): Promise<string | null> => {
+  try {
+    const response = await client.get("/generate-item");
+    if (response.data.success) {
+      return response.data.item;
+    } else {
+      return null;
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+export { serverStatus, generateItem };
